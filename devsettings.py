247d492974982caebfaa7b32dd8d03f25f8a6138
Ionel Cristian Mărieș <contact@ionelmc.ro>
2015-04-15 03:22:02 +0300

    Add missing devsettings.

---
+++ devsettings.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+from settings import *
+
+####### DEBUG #######
+CACHE_CONTENT = False
+RELATIVE_URLS = True
+REAL_SITEURL = SITEURL = 'http://localhost'
+ASSET_DEBUG = True
+GOOGLE_ANALYTICS_ACCOUNT = 'UA-822534-16'
+#####################
