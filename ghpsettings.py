8634cf95adef6b16b993ebc108524865ac696818
Ionel Cristian Mărieș <contact@ionelmc.ro>
2015-05-10 02:11:44 +0300

    Adaugare setari pentru GitHub Pages

---
+++ ghpsettings.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+from settings import *
+
+####### DEBUG #######
+CACHE_CONTENT = False
+RELATIVE_URLS = True
+REAL_SITEURL = SITEURL = 'http://ropython.github.io/ropython-site'
+ASSET_DEBUG = False
+GOOGLE_ANALYTICS_ACCOUNT = '???'
+#####################
